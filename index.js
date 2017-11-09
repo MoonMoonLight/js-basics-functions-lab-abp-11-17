8be101a56710d24397e7ff7a5e050c50ab569b49
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-11-09 20:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ function distanceFromHqInFeet(){
   return distanceFromHqInBlocks * 264;
 }
 
-function calculatesFarePrice(){
-  distanceTravelledInFeet - 400
-  400< distanceTravelledInFeet < 2000 = 2 * distanceTravelledInFeet
+var distanceTravelledInFeet = distanceTravelledInFeet - 400;
+
+function calculatesFarePrice(distance, price){
+  if(400 < distanceTravelledInFeet < 2000){
+    price = distanceTravelledInFeet * 2;
+    return price;
+  }
   2000 < distanceTravelledInFeet < 2500 = 25;
-  distanceTravelledInFeet > 2500{
+  else if(distanceTravelledInFeet > 2500){
   return "Cannot travel that far";
 }
 
