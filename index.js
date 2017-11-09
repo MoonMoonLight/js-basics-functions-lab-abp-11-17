bc946ba07d55ddde9e936a6cae09bb7f95a03ae9
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-11-09 20:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(){
-  
+
 }
 function distanceFromHqInFeet(){
-  distanceFromHqInBlocks();
+  return distanceFromHqInBlocks * 264;
 }
 
-distanceTravelledInFeet
-
+distanceTravelledInFeet - 400
+400 2000 = 2 * distanceTravelledInFeet
+2000 2500 = flat fare
+distanceTravelledInFeet > 2500{
+  return "Cannot travel that far"
+}
 calculatesFarePrice
